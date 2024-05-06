2bcb70246c23a9e098f66ffc0955cce4edba0ddd
judson rian <judsrian@gmail.com>
2024-05-06 17:19:09 -0300

    updates de no projeto

--- components/CartSidebar.jsx
+++ components/CartSidebar.jsx
@@ -1,5 +1,26 @@
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "../components/ui/sheet";
+
 const CartSidebar = () => {
-  return <div>Cart Sidebar</div>;
+  return (
+    <Sheet open={true}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Minha Carteira</SheetTitle>
+          <p>Item 1</p>
+          <p>Item 2</p>
+          <p>Item 3</p>
+          <p>Item 4</p>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default CartSidebar;
