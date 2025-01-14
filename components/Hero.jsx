c8e305771cde959322b8194eccb69291c647866e
Judson Rian <judson_@outlook.com>
2025-01-13 22:27:34 -0300

    correções necessárias

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,15 +6,16 @@ const Hero = () => {
     <section className="py-40 px-20 md:py-10 md:h-[790px] relative overflow-hidden bg-white border-2">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
-          <div className="w-full xl:max-w-[580px] md:h-[620px] flex flex-col justify-center items-start">
+          <div className="w-full xl:max-w-[580px] md:h-[620px] flex flex-col justify-center items-center">
             <h1 className="text-center xl:text-left mb-6">
-              FAÇA O QUE <span>TE</span> FAZ FELIZ
+              COMEÇE 2025 COM ESTILO
             </h1>
             <p className="mb-10 text-lg max-w-[508px] mx-auto text-center xl:text-left xl:mx-0">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste
-              sint, maxime incidunt iusto quod cum ipsum nostrum nemo nihil est.
+              Todo recomeço mereçe um brilho único. As joias são desenhadas por
+              Mari, feitas com matéria prima de ótima qualidade, com um time
+              dedicado que harmoniza formas e estética perfeita...
             </p>
-            <div className="flex items-center gap-4 mx-auto xl:mx-0">
+            <div className="flex flex-col md:flex-row items-center gap-4 mx-auto xl:mx-0">
               <Link href={"/nossas-joias"} className="mx-auto md:mx-0">
                 <button className="btn btn-primary">Comprar Agora</button>
               </Link>
@@ -23,7 +24,7 @@ const Hero = () => {
               </Link>
             </div>
           </div>
-          <div className="hidden xl:flex ">
+          <div className="hidden md:flex">
             <Image
               src="/hero/joia.webp"
               width={600}
